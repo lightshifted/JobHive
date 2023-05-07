3955f06b77b92a913a8f27a89c7957c0dea500b0
masslightsquared <leptonneutrino@protonmail.com>
2023-05-07 11:11:36 -0400

    Add: thread management

--- stream.py
+++ stream.py
@@ -2,88 +2,80 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
 import json
-import asyncio
-import websockets
-import threading
 import queue
 
 from typing import List, Dict, Callable
 
 
-class ThreadedGenerator:
-    def __init__(self):
-        self.queue = queue.Queue()
+class FileEventHandler(FileSystemEventHandler):
+    """
+    A custom event handler for file system events.
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        item = self.queue.get()
-        if item is StopIteration: raise item
-        return item
-
-    def send(self, data):
-        self.queue.put(data)
-
-    def close(self):
-        self.queue.put(StopIteration)
-
-
-async def send_to_websocket(parsed_data: Dict):
-    uri = "ws://localhost:2000/ws"
-    async with websockets.connect(uri) as websocket:
-        await websocket.send(json.dumps(parsed_data))
-
-
-def push_to_client(data: ThreadedGenerator):
-    try:
-        print("Data loaded successfully: ", data.queue.get())
-        if data.queue.empty():
-            return "No data to send"
-    except json.JSONDecodeError:
-        raise ValueError('Invalid JSON data')
-    
-    data.close()
-    return
-
-
-class MyEventHandler(FileSystemEventHandler):
-    push_to_client: Callable[[List[str]], None]
-    send_to_websocket: Callable[[Dict], None]
+    Parameters:
+    -----------
+    data_queue : queue.Queue
+        A queue to send data from the file to a separate thread.
+    """
+    def __init__(self, data_queue: queue.Queue):
+        self.data_queue = data_queue
 
     def on_modified(self, event):
+        """
+        Handler function for file modification events.
+        """
         print(f"File modified: {event.src_path}")
 
     def on_created(self, event):
+        """
+        Handler function for file creation events.
+        """
         print(f"File created: {event.src_path}")
         # open json file and read contents
         with open(event.src_path, "r") as file:
             try:
                 data = json.load(file)
+                self.data_queue.put(data) # Send the data to a separate thread
             except json.JSONDecodeError:
                 print("JSONDecodeError")
-        g = ThreadedGenerator()
-        g.send(data)
-        threading.Thread(target=send_to_websocket, args=(g,)).start()
-        return push_to_client(g)
 
     def on_deleted(self, event):
+        """
+        Handler function for file deletion events.
+        """
         print(f"File deleted: {event.src_path}")
 
 
-def monitor_directory(path):
+data_queue = queue.Queue() # For threadsafe communication between threads
+
+def monitor_directory(path: str, data_queue: queue.Queue = data_queue):
+    """
+    Monitor a directory for file system events.
+
+    Parameters:
+    -----------
+    path : str
+        The path of the directory to monitor.
+    data_queue : queue.Queue, optional
+        A queue to send data from the file to a separate thread.
+    """
     print("Monitoring directory " + path)
-    event_handler = MyEventHandler()
+    event_handler = FileEventHandler(data_queue)
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
 
     try:
         while True:
+            try:
+                data = data_queue.get(timeout=1)
+                print("Data received: " + str(data))
+            except queue.Empty:
+                pass
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
+
 
 if __name__ == "__main__":
     directory_to_monitor = "./client_data/agent_interactions"
